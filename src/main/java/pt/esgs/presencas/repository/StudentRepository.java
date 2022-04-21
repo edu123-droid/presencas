afc220a92b1bacea88ac263b089318bfd802e0a9
Eduardo & Tiago <eduardo_ribeiro2003@hotmail.com>
2022-04-21 17:31:54 +0100

    File not present

--- src/main/java/pt/esgs/presencas/repository/StudentRepository.java
+++ src/main/java/pt/esgs/presencas/repository/StudentRepository.java
@@ -2,24 +2,57 @@ package pt.esgs.presencas.repository;
 
 import pt.esgs.presencas.model.Student;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StudentRepository {
 
     private final List<Student> studentList;
 
     public StudentRepository(){
-        studentList = new ArrayList<>();
-        studentList.add(Student.of(59239, "Filipe Silva"));
-        studentList.add(Student.of(59232, "Joaquim Silva"));
-        studentList.add(Student.of(59233, "Manel Silva"));
-        studentList.add(Student.of(59234, "Tiago Silva"));
+        studentList = loadStudentsFile()
+                .stream()
+                .map(s -> s.split(","))
+                .filter(s -> s.length > 1)
+                .map(s -> new Student(Long.parseLong(s[0]), s[1]))
+                .toList();
+
     }
 
     public List<Student> findAll(){
-        return Collections.unmodifiableList(studentList);
+        return new ArrayList<>(studentList);
+        //return Collections.unmodifiableList(studentList);
+    }
+
+    private List<String> loadStudentsFile(){
+        List<String> result = null;
+
+        try(InputStream inputStream = getClass().getClassLoader().getResourceAsStream("students.csv")) {
+
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputStream)
+            );
+
+
+
+            result = reader.lines().collect(Collectors.toList());
+            //inputStream.close();
+        } catch (IOException a){
+            System.err.println("Failed to close file");
+        }catch (NullPointerException b){
+            System.err.println("Cannot open file");
+            return Collections.emptyList();
+        }
+
+        return result;
     }
 }
